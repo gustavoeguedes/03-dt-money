f330afe0b6bccf701e260d4447435cc6fbf00e84
gustavoeguedes <gustavoestevesguedes12@gmail.com>
2025-05-25 20:57:05 -0300

    fix: update json-server version and refactor transaction handling

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -2,7 +2,7 @@ import * as Dialog from '@radix-ui/react-dialog'
 import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles'
 import { ArrowCircleUp, X } from 'phosphor-react'
 import { z } from 'zod'
-import { useForm } from 'react-hook-form'
+import { Controller, useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 
 const newTransactionFormSchema = z.object({
@@ -15,7 +15,7 @@ const newTransactionFormSchema = z.object({
 type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>
 
 export function NewTransactionModal() {
-    const {register, handleSubmit, formState: {isSubmitting}} = useForm<NewTransactionFormInputs>({
+    const {register, control, handleSubmit, formState: {isSubmitting}} = useForm<NewTransactionFormInputs>({
         resolver: zodResolver(newTransactionFormSchema),
     })
 
@@ -39,17 +39,22 @@ export function NewTransactionModal() {
                     <input type="number" placeholder='Preço' required {...register('price', {valueAsNumber: true})}  />
                     <input type="text" placeholder='Categoria' required {...register('category')}  />
 
-                    <TransactionType>
-                        <TransactionTypeButton variant='income' value='income'>
-                            <ArrowCircleUp size={24} />
-                            Entrada
-                        </TransactionTypeButton>
-                        <TransactionTypeButton variant='outcome' value='outcome'>
-                            <ArrowCircleUp size={24} />
-                            Saída
-                        </TransactionTypeButton>
-                    </TransactionType>
+                    <Controller control={control} name='type' render={({field}) => {
+                        
+                        return (
+                            <TransactionType onValueChange={field.onChange} value={field.value}>
+                                <TransactionTypeButton variant='income' value='income'>
+                                    <ArrowCircleUp size={24} />
+                                    Entrada
+                                </TransactionTypeButton>
+                                <TransactionTypeButton variant='outcome' value='outcome'>
+                                    <ArrowCircleUp size={24} />
+                                    Saída
+                                </TransactionTypeButton>
+                            </TransactionType>
 
+                        )
+                    }} />
                     <button type='submit' disabled={isSubmitting}>
                         Cadastrar
                     </button>
--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -11,25 +11,32 @@ interface Transaction {
 
 interface TransactionContextType {
     transactions: Transaction[];
+    fetchTransactions: (query?: string) => Promise<void>;
 }
 
 export const TransactionsContext = createContext({} as TransactionContextType);
 
 export function TransactionsProvider({ children }: { children: React.ReactNode }) {
     const [transactions, setTransactions] = useState<Transaction[]>([])
-        async function loadTransactions() {
-            const response = await fetch('http://localhost:3333/transactions')
+        async function fetchTransactions(query?: string) {
+            const url = new URL('http://localhost:3333/transactions')
+            
+            if (query) {
+                url.searchParams.append('q', query)
+            }
+            console.log(url.toString())
+            const response = await fetch(url)
             const data = await response.json()
             setTransactions(data)
             console.log(data)
         }
     
         useEffect(() => {
-            loadTransactions()
+            fetchTransactions()
             
         }, [])
     return (
-        <TransactionsContext.Provider value={{ transactions }}>
+        <TransactionsContext.Provider value={{ transactions,fetchTransactions }}>
             {children}
         </TransactionsContext.Provider>
     )
