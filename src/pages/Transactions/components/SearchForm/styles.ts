3ee619c3e8dcd7db2e0cb9283f6e1c4016ed30ad
gustavoeguedes <gustavoestevesguedes12@gmail.com>
2025-05-25 18:27:10 -0300

    feat: integrate react-hook-form and zod for form validation in NewTransactionModal and SearchForm components

--- src/pages/Transactions/components/SearchForm/styles.ts
+++ src/pages/Transactions/components/SearchForm/styles.ts
@@ -31,7 +31,12 @@ export const SearchFormContainer = styled.form`
         border-radius: 6px;
         cursor: pointer;
 
-        &:hover{ 
+        &:disabled {
+            opacity: 0.7;
+            cursor: not-allowed;
+        }
+
+        &:not(:disabled):hover{ 
             background-color: ${props => props.theme['green-500']};
             color: ${props => props.theme['white']};
             border-color: ${props => props.theme['green-500']};
